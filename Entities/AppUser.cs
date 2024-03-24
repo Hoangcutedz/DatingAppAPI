76999cf1adfcaf3eebf0d0b075813ce234244f04
Hoangcutedz <hoangn2422@gmail.com>
2024-03-24 13:10:08 +0700

    Add project files.

---
+++ Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; } // column of table in data base, primary key
+        public string UserName { get; set; } // column of table in data base
+    }
+}
