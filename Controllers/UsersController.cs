ee2cd96cf7421468968437d95a7b32244599ced9
Hoangcutedz <hoangn2422@gmail.com>
2024-04-06 15:10:00 +0700

    end of section 10

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -18,7 +19,7 @@ namespace API.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet] // endpoint /api/userss
+        [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             var users = await _userRepository.GetMenbersAsync();
@@ -26,10 +27,25 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        [HttpGet("{username}")] // endpoint /api/users/2
+        [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            if (user == null) return NotFound();
+
+            _mapper.Map(memberUpdateDto, user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+            
+            return BadRequest("Failed to update user");
         }
     }
 }
